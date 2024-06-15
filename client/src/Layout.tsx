7a72e260d17024a8e30b7f58faf2b07078e1c3c7
Frida Alstergren <frida@alstergren.com>
2024-06-15 14:34:54 +0200

    123

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
-// import { Navbar } from "./components/Navbar";
+import { Navbar } from "./components/Navbar";
 // import { Footer } from "./components/Footer";
 
 export const Layout = () => {
   return (
     <main>
-      {/* <Navbar /> */}
+      <Navbar />
       <Outlet />
       {/* <Footer /> */}
     </main>
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+
+export const Navbar = () => {
+  return (
+    <div className="nav-contatiner">
+      <div className="menu-icon">
+      </div>
+      <div className="logo-container">
+        <h1 id="logo">AGONiST</h1>
+      </div>
+    </div>
+  );
+};
